3ea6cb4474069735ac66ebfa1cadb2c5aff256c1
BenneJam <benji.marki70@gmail.com>
2026-01-21 08:52:15 +0100

    modification-projets

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,59 @@
 (function ($) {
+  "use strict";
 
-	"use strict";
+  $(".main-menu a").click(function () {
+    var id = $(this).attr("class");
+    id = id.split("-");
+    $("a.active").removeClass("active");
+    $(this).addClass("active");
+    $("#menu-container .content").slideUp("slow");
+    $("#menu-container #menu-" + id[1]).slideDown("slow");
+    $("#menu-container .homepage").slideUp("slow");
+    return false;
+  });
 
-	$(".main-menu a").click(function () {
-		var id = $(this).attr('class');
-		id = id.split('-');
-		$('a.active').removeClass('active');
-		$(this).addClass('active');
-		$("#menu-container .content").slideUp('slow');
-		$("#menu-container #menu-" + id[1]).slideDown('slow');
-		$("#menu-container .homepage").slideUp('slow');
-		return false;
-	});
+  $(".main-menu a.homebutton").click(function () {
+    $("#menu-container .content").slideUp("slow");
+    $("#menu-container .homepage").slideDown("slow");
+    return false;
+  });
 
+  $(".main-menu a.infobutton").click(function () {
+    $("#menu-container .content").slideUp("slow");
+    $("#menu-container .info-section").slideDown("slow");
+    return false;
+  });
 
-	$(".main-menu a.homebutton").click(function () {
-		$("#menu-container .content").slideUp('slow');
-		$("#menu-container .homepage").slideDown('slow');
-		return false;
-	});
+  $(".main-menu a.projectbutton").click(function () {
+    $("#menu-container .content").slideUp("slow");
+    $("#menu-container .gallery-section").slideDown("slow");
+    return false;
+  });
 
-	$(".main-menu a.infobutton").click(function () {
-		$("#menu-container .content").slideUp('slow');
-		$("#menu-container .info-section").slideDown('slow');
-		return false;
-	});
+  $(".main-menu a.twatchbutton").click(function () {
+    $("#menu-container .content").slideUp("slow");
+    $("#menu-container .twatch-section").slideDown("slow");
+    return false;
+  });
 
-	$(".main-menu a.projectbutton").click(function () {
-		$("#menu-container .content").slideUp('slow');
-		$("#menu-container .gallery-section").slideDown('slow');
-		return false;
-	});
+  $(".toggle-menu").click(function () {
+    $(".show-menu").stop(true, true).slideToggle();
+    return false;
+  });
 
-	$(".main-menu a.twatchbutton").click(function () {
-		$("#menu-container .content").slideUp('slow');
-		$("#menu-container .twatch-section").slideDown('slow');
-		return false;
-	});
+  $(".show-menu a").click(function () {
+    $(".show-menu").fadeOut("slow");
+  });
 
-	$('.toggle-menu').click(function () {
-		$('.show-menu').stop(true, true).slideToggle();
-		return false;
-	});
-
-	$('.show-menu a').click(function () {
-		$('.show-menu').fadeOut('slow');
-	});
-
-
+  // Toggle additional projects
+  $("#toggle-projects").click(function () {
+    var $additionalProjects = $("#additional-projects");
+    if ($additionalProjects.is(":visible")) {
+      $additionalProjects.slideUp("slow");
+      $(this).text("Voir plus");
+    } else {
+      $additionalProjects.slideDown("slow");
+      $(this).text("Voir moins");
+    }
+  });
 })(jQuery);
